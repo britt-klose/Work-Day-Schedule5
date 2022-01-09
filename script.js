3e2b0f86ebb84d10b2076afd5e94d511fa1e97ea
brittany klose <brittany.klose@yahoo.com>
2022-01-08 21:34:01 -0500

    adding ids to buttons

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 //open with current date
-var currentDate= moment().format("dddd, MMMM Do YYYY");
-var currentHour=moment().format("HH");
+var currentDate = moment().format("dddd, MMMM Do YYYY");
+var currentHour = moment().format("HH");
 console.log(currentDate)
 console.log(currentHour)
-var hour=document.querySelectorAll(".hour")
+var hour = document.querySelectorAll(".hour")
 var state = document.querySelectorAll(".description")
 console.log(state)
-var saveButtons=document.querySelectorAll(".saveBtn")
-document.getElementById("currentDay").textContent=currentDate
+var saveButtons = document.querySelectorAll(".saveBtn")
+document.getElementById("currentDay").textContent = currentDate
 
 
 
 //Ifs for past present future
-for (var i=0; i<state.length; i ++){
-    if (hour[i].textContent.substring(0,2)<currentHour){
+for (var i = 0; i < state.length; i++) {
+    if (hour[i].textContent.substring(0, 2) < currentHour) {
         state[i].setAttribute(
-        "style", "background-color:gray"
-    )
-    } else if (hour[i].textContent.substring(0,2)>currentHour){
+            "style", "background-color:gray"
+        )
+    } else if (hour[i].textContent.substring(0, 2) > currentHour) {
         state[i].setAttribute(
-        "style", "background-color: blue"
-    )} else {
+            "style", "background-color: blue"
+        )
+    } else {
         state[i].setAttribute(
             "style", "background-color: green"
         )
@@ -28,35 +29,18 @@ for (var i=0; i<state.length; i ++){
 }
 
 //save buttons
-//var buttons=document.querySelectorAll(".saveBtn")
-//console.log(buttons)
-//for (var i=0; i<buttons.length; i++){}
-//buttons.addeventlistner("click", function(){})
+var buttons = document.querySelectorAll(".saveBtn")
+console.log(buttons)
+for (var i = 0; i < buttons.length; i++) { 
+    buttons[i].setAttribute
+    buttons[i].addEventListener("click", function () { 
+        console.log()
+    }) 
+}
+
+
+
 
 //for loop buttons [i].addeventlistener (cuz cant do evnt lstnr to array)
 //adding idiv evnt lstnrs. loop makes array seperate/indiv
 
-function saveTodos(){
-    var tasks={
-      comment: comment.value.trim()
-    };
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-}
-function renderTodos(){
-    var savedTask =JSON.parse(localStorage.getItem("tasks"));
-    if (savedTask!==null){
-        document.querySelectorAll(".description col-10").innerhtml = savedTask.comment;
-    } else{
-        return;
-    }
-}
-saveButtons.addEventListener("click", function(event){
-    event.preventDefault();
-    saveTodos();
-    (renderTodos);
-});
-
-function init(){
-    renderTodos();
-}
-init();
